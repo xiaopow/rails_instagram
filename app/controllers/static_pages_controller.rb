4dc27acc9e66f6599e2ff32ec68f5acb9f5bb09b
Xiaopow <xiaoyang88@gmail.com>
2015-05-06 15:19:19 +0800

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  def index
+    # Since we don't have any logics needed, we are going to leave this empty
+
+    # numbers = [1,2,3,4]
+
+    # render :json => {
+    #   person: { name: "fer" },
+    #   lucky_numbers: numbers
+    # }
+  end
+end
